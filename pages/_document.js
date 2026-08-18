799e6364bc8133d010870041170dd2675742b083
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2026-08-18 16:49:03 +0600

    document file updated

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,400;0,9..144,500;0,9..144,600;0,9..144,700;1,9..144,500&family=Public+Sans:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="theme-color" content="#0a1830" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
